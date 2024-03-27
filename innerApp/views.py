5713dbbea348783db05d7434b69a7bdfb42a2e6e
jason <jjy714@gmail.com>
2024-03-27 17:18:03 +0900

    first commit
    Signed-off-by: jason <jjy714@gmail.com>

---
+++ innerApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import foods, questions
+# Create your views here.
+
+
+def home(request):
+    return render(request, "innerApp/home.html", {})
+
+
+def question_ask(response):
+    ls = questions.objects.get(id=1)
+    return render(response, "innerApp/list.html", {'questions' : questions.question})
+
+
+def about_us(response):
+    return render(response, "innerApp/aboutus.html")
